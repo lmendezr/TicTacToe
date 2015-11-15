98dcec0813a45a52b6016370bcb23d226bd97bb4
Andrew1991 <nestor.morales2@upr.edu>
2015-11-14 21:18:50 -0400

    done complete

--- done.v
+++ done.v
@@ -12,5 +12,6 @@ supply0 ground;
 		d_flip_flop d1(player1_win_indicate, out2, reset, ground, player1_win);
 		not(out1, player1_win_indicate); //q negado a d2
 		d_flip_flop d2(player2_win_indicate, out1, reset, ground, player2_win);
-		not(out2, player2_win_indicate);
+		not(out2, player2_win_indicate); 
+		
 endmodule
